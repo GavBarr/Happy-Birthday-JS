08b6bad3447066e1d017e9d2f6ec475aef51266a
GavBarr <107496821+GavBarr@users.noreply.github.com>
2022-07-21 18:57:26 -0600

    Add files via upload

---
+++ js-birthday/js/script.js
@@ -0,0 +1,4 @@
+var name = prompt("What's your name? " );
+var age = prompt("How old are you? " );
+var month = prompt("What month were you born in? ");
+alert("How's it going " + name + "!" + " It looks like you're " + age + " years old and you were born in " + month + "!");
